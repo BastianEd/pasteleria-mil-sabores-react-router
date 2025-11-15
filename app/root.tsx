a8ba9124e0cc5e6bd4ed0cfd9e5acbe9582a9039
BastianEd <ba.rubiom@duocuc.cl>
2025-11-15 15:21:19 -0300

    No hay vida..

--- app/root.tsx
+++ app/root.tsx
@@ -50,14 +50,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="es">
         <head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <Meta />
-            <Links />
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+            <link rel="shortcut icon" href="https://i.ibb.co/p6Wd9mp4/Icon.png"/>
         </head>
         <body>
         {children}
-        <ScrollRestoration />
+        <ScrollRestoration/>
         <Scripts />
         </body>
         </html>
